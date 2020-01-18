1a871fe2c7f6ccfad905fa53b9e9d72390c69b04
SeungHun Han <shhan730@gmail.com>
2020-01-19 07:38:49 +0900

    Set index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
 
-app.get('/users',(req,res)=> {
-  res.send('user list');
+var users = [
+  {id: 1, name: 'A'},
+  {id: 2, name: 'B'},
+  {id: 3, name: 'C'},
+]
+app.use(morgan('dev'));
+
+app.get('/users',(req,res) => {
+  res.json(users);
 });
 
 app.listen(3000,function(){
