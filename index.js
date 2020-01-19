2905af0546f0a22665fb7b3c8d795d9c36c6ec5d
SeungHun Han <shhan730@gmail.com>
2020-01-19 09:05:31 +0900

    set DELETE /user/:id

--- index.js
+++ index.js
@@ -23,8 +23,17 @@ app.get('/users/:id',(req,res) => {
 
   const user = users.filter((user) => user.id === id)[0];
   if(!user) return res.status(404).end();
-  
+
   res.json(user);
+})
+
+app.delete('/users/:id',(req,res) =>{
+  const id = parseInt(req.params.id, 10);
+  if(Number.isNaN(id)) return res.status(400).end();
+
+  users = users.filter((user) => user.id !== id);
+  res.status(204).end();
+
 })
 
 app.listen(3000,function(){
