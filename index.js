b3628f9a0f85a531a9b5bafd789b464f3e19eca6
SeungHun Han <shhan730@gmail.com>
2020-01-21 02:56:15 +0900

    Set POST /users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 var users = [
   {id: 1, name: 'A'},
@@ -8,6 +9,8 @@ var users = [
   {id: 3, name: 'C'},
 ]
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/users',(req,res) => {
   req.query.limit = req.query.limit || 10;
@@ -15,7 +18,7 @@ app.get('/users',(req,res) => {
 
   if(Number.isNaN(limit)){ return res.status(400).end() }
   res.json(users.slice(0,limit));
-});
+})
 
 app.get('/users/:id',(req,res) => {
   const id = parseInt(req.params.id, 10);
@@ -27,7 +30,7 @@ app.get('/users/:id',(req,res) => {
   res.json(user);
 })
 
-app.delete('/users/:id',(req,res) =>{
+app.delete('/users/:id',(req,res) => {
   const id = parseInt(req.params.id, 10);
   if(Number.isNaN(id)) return res.status(400).end();
 
@@ -35,6 +38,22 @@ app.delete('/users/:id',(req,res) =>{
   res.status(204).end();
 
 })
+
+app.post('/users',(req,res) => {
+  const name = req.body.name;
+  if(!name) return res.status(400).end();
+
+  const isConfict = users.filter((user) => user.name === name).length;
+  if(isConfict) return res.status(409).end();
+
+
+  const id = Date.now();
+  const user = {id, user};
+  users.push(user);
+  res.status(201).json(user);
+
+})
+
 
 app.listen(3000,function(){
   console.log('Server Running: 3000');
